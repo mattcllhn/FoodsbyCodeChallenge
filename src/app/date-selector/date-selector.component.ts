b9cee37cb9e4078edca2489cdb704cebd6744fb4
matt <mattcllhn@gmail.com>
2018-06-18 21:21:34 -0500

    js date hooked up

--- src/app/date-selector/date-selector.component.ts
+++ src/app/date-selector/date-selector.component.ts
@@ -8,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class DateSelectorComponent implements OnInit {
 
   constructor() { }
+  private daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  private dt = new Date();
+  private currentDateObj = new Date(this.dt);
+  private todayIndex = this.currentDateObj.getDay();
+  public today;
 
   ngOnInit() {
+    this.today = this.daysOfWeek[this.todayIndex];
+    console.log(this.dt, this.currentDateObj, this.todayIndex, this.today);
+
   }
 
 }
