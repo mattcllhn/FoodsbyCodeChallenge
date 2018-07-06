3c432a8947e817297c1fe7d8319cf050d7518c5e
matt <mattcllhn@gmail.com>
2018-07-06 14:15:53 -0500

    adding basic tests in spec files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,23 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { DateSelectorComponent } from './date-selector/date-selector.component';
+import { FlteredDisplayComponent } from './fltered-display/fltered-display.component'; 
+import { DataGetterService } from './data-getter.service'; 
+import { HttpModule } from '@angular/http';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
+      imports:[
+        HttpModule
       ],
+      declarations: [
+        AppComponent, 
+        DateSelectorComponent,
+        FlteredDisplayComponent
+      ],
+      providers: [DataGetterService],
+
     }).compileComponents();
   }));
   it('should create the app', async(() => {
--- src/app/data-getter.service.spec.ts
+++ src/app/data-getter.service.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed, inject } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpModule } from '@angular/http';
 import { DataGetterService } from './data-getter.service';
+const sampleJson = require('../assets/deliveries-sample.json');
 
 describe('DataGetterService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpModule,
+        HttpClientTestingModule
+      ],
       providers: [DataGetterService]
     });
   });
@@ -12,4 +18,11 @@ describe('DataGetterService', () => {
   it('should be created', inject([DataGetterService], (service: DataGetterService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('#getData should return the contents of static json file', inject([DataGetterService], (service: DataGetterService) => {
+    console.log('in the spec file', sampleJson);
+    service.getData().subscribe(res => { 
+      expect(res.data).toEqual(sampleJson)
+    });
+  }))
 });
